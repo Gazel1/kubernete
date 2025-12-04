e9e55ef3e41563d9f0bf4a55f36f5e0fb6772d16
Gazel1 <alexjimenezgarcia7@gmail.com>
2025-12-04 23:48:44 +0100

    VS

---
+++ ejercicio-2/terraform.tfvars
@@ -0,0 +1,4 @@
+db_password = "securepassword"
+db_user     = "matomo"
+db_name     = "matomodb"
+matomo_image = "alexjg7/matomo-custom:latest"
---
+++ ejercicio-2/variables.tf
@@ -0,0 +1,4 @@
+variable "db_password" { type = string }
+variable "db_user" { type = string }
+variable "db_name" { type = string }
+variable "matomo_image" { type = string }
