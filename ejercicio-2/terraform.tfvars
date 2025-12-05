3ff217f31079dc88b2588cb7c5a2eaef3285d1f9
Gazel1 <126425402+Gazel1@users.noreply.github.com>
2025-12-05 11:07:22 +0100

    Delete ejercicio-2 directory

--- ejercicio-2/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-db_password = "securepassword"
-db_user     = "matomo"
-db_name     = "matomodb"
-matomo_image = "alexjg7/matomo-custom:latest"
--- ejercicio-2/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "db_password" { type = string }
-variable "db_user" { type = string }
-variable "db_name" { type = string }
-variable "matomo_image" { type = string }
